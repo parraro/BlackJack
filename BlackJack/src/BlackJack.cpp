a2bf38cf8e072995dee28fb63bb87de3af0d9e69
Esteban Parra <estebanpr014@gmail.com>
2017-09-28 14:27:49 -0400

    Project creation successful

---
+++ BlackJack/src/BlackJack.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : BlackJack.cpp
+// Author      : Esteban Parra
+// Version     :
+// Copyright   : 
+// Description : BlackJack simulator in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
