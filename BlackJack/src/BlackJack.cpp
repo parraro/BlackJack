fd7b470913ab2e720d168e527c1164e3e75643a8
Esteban Parra <estebanpr014@gmail.com>
2017-09-28 16:02:51 -0400

    Random card creation implemented

--- BlackJack/src/BlackJack.cpp
+++ BlackJack/src/BlackJack.cpp
@@ -7,9 +7,15 @@
 //============================================================================
 
 #include <iostream>
+#include "card.cpp"
+
 using namespace std;
 
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	for(int i=0; i<10; i++){
+		Card c;
+		c.set_value(rand()%10);
+	}
 	return 0;
 }
---
+++ BlackJack/src/card.cpp
@@ -0,0 +1,17 @@
+/*
+ * dealer.cpp
+ *
+ *  Created on: Sep 28, 2017
+ *      Author: estebanparra
+ */
+
+class Card {
+    int value;
+  public:
+    void set_value (int);
+} card;
+
+void Card::set_value(int x){
+	value = x;
+}
+
